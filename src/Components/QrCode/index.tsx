e1d79b412e6be222c2aba54d04f966c82b32a98f
Igor Gomes <igorgomes255@gmail.com>
2023-03-28 15:01:35 -0300

    feat: mudando logica

--- src/Components/QrCode/index.tsx
+++ src/Components/QrCode/index.tsx
@@ -2,10 +2,14 @@ import { Container } from "./styles";
 import QRCode from "react-qr-code";
 import { useQRCodeContext } from "../../contexts/qrCodeContext";
 import { Card, CardBody } from "reactstrap";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const QrCode = () => {
   const { link, handleCaptureClick } = useQRCodeContext();
+
+  let { user } = useParams();
+
+  user = link;
 
   return (
     <Container>
@@ -13,7 +17,7 @@ const QrCode = () => {
         <CardBody>
           <h4>{link}</h4>
           <h5>Scan Me</h5>
-          <QRCode value={`https://desafio-qrcode-phi.vercel.app/${link}`} />
+          <QRCode value={`https://desafio-qrcode-phi.vercel.app/${user}`} />
         </CardBody>
       </Card>
 
